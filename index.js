7004dc8158e56394c528d88922d670942307ce44
Damien <damienlhaurado@hotmail.com>
2021-12-12 18:20:24 +0100

    go to react

--- index.js
+++ index.js
@@ -2,15 +2,23 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const fs = require("fs");
+const path = require("path");
 
 console.log("test")
 
+// app.get("/", (req,res, next) => {
+//     fs.readFile(__dirname + '/index.html', 'utf8', (err, text) => {
+//         return res.send(text);
+//     });
+// });
+app.use(express.static(path.join(_dirname, 'public')));
+
+
 app.get("/", (req,res, next) => {
-    fs.readFile(__dirname + '/index.html', 'utf8', (err, text) => {
-        return res.send(text);
-    });
-    
+    return res.sendFile(path.join(__dirname, './index.html'));
 });
+
+
 // fourni par heroku : process.env.port
 app.listen(port, () => {
     console.log("App running on port :" + port)
--- puissance4.js
+++ puissance4.js
@@ -1,3 +1,4 @@
+let turn = 0;
 var map = [
     [0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0],
@@ -17,13 +18,19 @@ for (i = 0; i <rows; i++) {
         if(map[i][j] == 0) grille += "<div class='case' data-rows='"+i+"' data-columns='"+j+"'></div>";
         if(map[i][j] == 1) grille += "<div class='case red' data-rows='"+i+"' data-columns='"+j+"'></div>"
         if(map[i][j] == 2) grille += "<div class='case blue' data-rows='"+i+"' data-columns='"+j+"'></div>"
-        
     }
     grille += "</div>";
 }
 $("#game").html(grille)
 
 $(".case").click(function(e) {
-    
-    console.log($(e.target).prop('id'))
+    turn == 0 ? turn = 1 : turn = 0
+    //console.log("ligne : "+ $(e.target).data("rows"));
+    //console.log("collone : "+ $(e.target).data("columns"));
+    console.log()
+    // pour une collones cliquée, chercher la valeur de ligne la plus basse 
+    $.each($('div[data-columns='+$(e.target).data("columns")+']'), function(i, e) {
+        console.log('name='+ i + ' value=' +e);
+    });
+    //console.log("turn : " + turn)
 });
