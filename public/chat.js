2b80d9c0beda5a3ffadeb62363b4eaff48289b8b
merrodev <damienlhaurado@hotmail.com>
2021-12-16 15:09:46 +0100

    commit before merging from master

--- public/chat.js
+++ public/chat.js
@@ -1,8 +1,38 @@
 class Chat extends React.Component {
+    constructor(props) {
+        super();
+        this.myRef = React.createRef();
+        this.listMessage = React.createRef();
+    }
+    send = () => {
+        const node = this.myRef.current.value;
 
+        console.log("Ok msg");
+        console.log(node)
+        socket.emit("messageSend", node)
+
+    }
+
+   
+    
+    render() {
+        return (
+            <div className="Chat">
+                <div id="zone-text">
+                <input type="text" id="ref" ref={this.myRef} ></input>
+                <button id="send" onClick={() => {this.send()}} >Send</button>
+                </div>
+                
+                <div id="message" ref={this.listMessage}>
+                    test message
+                </div>
+
+            </div>
+        );
+    }
 }
 
-// ReactDOM.render(
-//     <App />,
-//     document.getElementById('root')
-//   );
+ReactDOM.render(
+    <Chat />,
+    document.getElementById('chat')
+  );
