c06d4d302c36f07a4f797f6f79c7ef017e0af563
luc-lecoutour <77966986+luc-lecoutour@users.noreply.github.com>
2021-12-16 15:19:44 +0100

    make it better

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const port = process.env.PORT || 3000;
 const http = require("http").Server(app);
 //const io = require("socket.io")(http);
 
-const Player = require("./public/Player.js");
+const {Player} = require("./public/Player");
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(err, req, res, next) {
